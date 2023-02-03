24f130f09b9f20dd566ee315c016a7083d57ab27
anukul-mahato <anukulmahato014@gmail.com>
2023-02-03 14:55:43 +0600

    6 Variable creat for my assignment

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Product upload form for three HP monitors
+
+// Variables for monitor specifications
+$displaySize = 15.6; // data type: float
+$vgaCable = true; // data type: boolean
+$price = 8000; // data type: integer
+$brand = "HP"; // data type: string
+$model = "Unknown"; // data type: string
+$quantity = 3; // data type: integer
+
+// Code to insert these variables into a database or output as a formatted product listing
+
+
